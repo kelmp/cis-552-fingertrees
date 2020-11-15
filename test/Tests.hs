f0d33abef69a86d3d722ced4f0029c6d1416c49e
Adrian Wang <wangadr@seas.upenn.edu>
2020-11-15 15:00:06 -0500

    Basic test structure

---
+++ test/Tests.hs
@@ -0,0 +1,35 @@
+import Test.HUnit
+import Test.QuickCheck
+import FingerTree
+
+testAll :: IO ()
+testAll = do
+  _ <-
+    runTestTT
+      ( TestList [
+        tConstruct,
+        tInsertHead,
+        tInsertTail,
+        tHead,
+        tTail,
+        tIsEmpty,
+        tConcat,
+        tSplit,
+        tMap,
+        tToList
+      ])
+
+quickCheckN :: Test.QuickCheck.Testable prop => Int -> prop -> IO ()
+quickCheckN n = quickCheckWith $ stdArgs {maxSuccess = n, maxSize = 100}
+
+verboseCheckN :: Test.QuickCheck.Testable prop => Int -> prop -> IO ()
+verboseCheckN n = verboseCheckWith $ stdArgs {maxSuccess = n, maxSize = 100}
+
+tConstruct :: Test
+tConstruct = TestList [
+  "Empty tree" ~: undefined,
+  "Unit tree" ~: undefined,
+  "Larger tree" ~: undefined]
+
+tInsertHead :: Test
+tInsertHead = undefined
