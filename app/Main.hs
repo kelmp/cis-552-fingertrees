e086b832ab9f177358a825d6cd1c23c5720d18ad
Adrian Wang <wangadr@seas.upenn.edu>
2020-12-07 01:17:04 -0500

    More progress on random access application

--- app/Main.hs
+++ app/Main.hs
@@ -6,7 +6,8 @@ import Text.Read (readMaybe)
 
 newtype Person = Person {
     name :: String
-    bio :: String
+    adj :: String
+    memberFor :: String
 }
 
 durTree :: FingerTree String
@@ -31,10 +32,9 @@ newPerson = do
   let name = nameTree !! nameI
       adj = adjTree !! adjI
       dur = durTree !! durI in
-  return { name = name
-    bio = name ++ " has been a " ++ adj ++ " customer of Not-Disney for " ++
-      time ++ " " ++ dur ++ ", but that's going to change if they can't speak\
-        \to a manager RIGHT NOW."}
+  return { name = nameTree !! nameI
+    adj = adjTree !! adjI
+    memberFor = show time ++ " " ++ durTree !! durI}
 
 addPerson :: Bool -> FingerTree Person -> FingerTree Person
 addPerson fastpass ft = do
@@ -44,14 +44,36 @@ addPerson fastpass ft = do
 init :: Int -> FingerTree Person
 init n = FT.fromList $ iterate (addPerson False) Nil !! n
 
+complain :: Person -> String
+complain p = name p ++ " has been a " ++ adj p ++ " customer of Not-Disney for "
+  ++ time ++ " " ++ dur ++ ", but that's going to change if they can't speak \
+        \to a manager RIGHT NOW."
+
+
+
 getInt = do
   input <- getLine
   case (readMaybe input :: Maybe Int) of
     Just i -> i
     Nothing -> getInt
 
+queueLoop :: FingerTree Person -> IO ()
+queueLoop q = let len = length q in do
+  putStrLn "There are currently " ++ show len ++ " people in the queue."
+  angryI <- randomRIO (0, length q - 1)
+  putStrLn . complain . q !! angryI
+  putStrLn "Please type your response to this well-behaved customer."
+  input <- getLine
+  putStrLn "After hearing your soothing words, the customer heads back to their\
+  \ spot in line."
+  putStrLn "How many people would you like to let into the ride?"
+  let admit = getInt
+  -- get input, bound it by length, let that many people in.
+  -- add 1-10 people to line
+  queueLoop
 
 main :: IO ()
 main = do
   putStrLn "Welcome to Not-Disney Not-Land-Or-World! How many people have \
   \rushed into the renowned Cosmos-Themed Landform Ride as soon as it opened?"
+  let queue = init getInt
